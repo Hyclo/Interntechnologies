8e6ba3630c20ec34dd2ea0f8dd6240ac2413e168
Hyclo <miguel-aroldi@bluewin.ch>
2022-11-17 15:19:25 +0100

    second commit

---
+++ src/main/java/ch/intech/website/InterntechnologiesApplication.java
@@ -0,0 +1,13 @@
+package ch.intech.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterntechnologiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterntechnologiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/intech/website/InterntechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.intech.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterntechnologiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
